7843328023c2cceca3ae45f9543df6f73f3d735a
DannyMart18 <58661521+DannyMart18@users.noreply.github.com>
2021-03-17 19:35:40 +0000

    Part 2 nearly working - various improvements, sorting working

---
+++ src/part03/MP3PlayerTest.java
@@ -0,0 +1,41 @@
+package part03;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MP3PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getTuneInfo() throws Exception {
+
+    }
+
+    @Test
+    void testGetTuneInfo() {
+    }
+
+    @Test
+    void testGetTuneInfo1() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void addTune() {
+    }
+
+    @Test
+    void switchOff() {
+    }
+
+    @Test
+    void switchOn() {
+    }
+}
